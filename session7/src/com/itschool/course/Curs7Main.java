d9f07a2c6267ff46d6e5fc8c71214cef76d77758
Ga3r1el <vajda.gabriel.edgar@student.ms.sapientia.ro>
2024-03-18 11:54:44 +0200

    tma

---
+++ session7/src/com/itschool/course/Curs7Main.java
@@ -0,0 +1,89 @@
+package com.itschool.course;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Curs7Main {
+//COURSE 5-6 HOMEWORK
+//EX 1.
+//        public static void main(String[] args) {
+//            String stringOne = "OpenAI";
+//            String stringTwo = "OpenAI";
+//
+//            boolean areSameObject = (stringOne == stringTwo);
+//
+//            System.out.println("Do stringOne and stringTwo refer to the same object? " + areSameObject);
+//        }
+//    }
+
+//EX 3.
+//    public static void main(String[] args) {
+//        int age = 20;
+//        if (age >= 18) {
+//            System.out.println("I am an adult.");
+//        } else {
+//            System.out.println("I am not an adult.");
+//        }
+//    }
+//}
+//    public static void main(String[] args) {
+//        int x = 5;
+//        double y = 5.1;
+//        System.out.println(x == y);
+//        boolean a = true;
+//        boolean b = false;
+//        System.out.println();
+//    }
+//}
+
+////EX 4.
+//
+//    public static void main(String[] args) {
+//        int heightOne = 190;
+//        int heightTwo = 180;
+//        int maximumHeight = (heightOne > heightTwo) ? heightOne : heightTwo;
+//
+//        System.out.println("The maximum height is: " + maximumHeight);
+//    }
+//}
+
+////EX 5.
+//    public static void main(String[] args) {
+//        System.out.println("Numbers from 1 to 10:");
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println(i + "");
+//        }
+//    }
+//}
+//    public static void main(String[] args) {
+//        System.out.println("Even numbers from 1 to 10:");
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 == 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+//    }
+//}
+
+//COURSE 7 HOMEWORK
+//EX 1.
+//    public static void main(String[] args) {
+//int i = 123;
+//        System.out.println(i % 2 == 0 ? "even" : "odd");
+//    }
+//}
+
+    //    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int number = scanner.nextInt();
+//        scanner.close();
+//
+//        if (number % 2 == 0) {
+//            System.out.println("Even");
+//        } else {
+//            System.out.println("Odd");
+//        }
+//    }
+}
